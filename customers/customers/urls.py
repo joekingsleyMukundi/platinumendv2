d345110248a60409a5e662ea9277068a51ba0a1a
joekingsley <joekingsleymukundi@gmail.com>
2022-07-19 10:47:06 +0300

    chore: add nginx to customers service

--- customers/customers/urls.py
+++ customers/customers/urls.py
@@ -14,8 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('api/v1/customers/client', include('client.urls')),
+    # path('api/v1/customer/freelancer', include('freelancer.urls')),
+    # path('api/v1/customer/workers', include('workers.urls')),
+    # path('api/v1/customer/employers', include('employers.urls')),
 ]
